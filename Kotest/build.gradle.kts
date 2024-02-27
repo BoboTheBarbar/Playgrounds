680ec4c7153d92ec1915b459dd75cb2389f71e9f
Boris Prochnau <boris.prochnau@colenio.com>
2024-02-27 12:37:31 +0100

    bootstrap initial setup

---
+++ Kotest/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    kotlin("jvm") version "1.9.22"
+}
+
+group = "com.kotlin"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+val koTestCoreVersion = "5.8.0"
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+    // Kotest
+    testImplementation("io.kotest:kotest-runner-junit5:$koTestCoreVersion")
+    testImplementation("io.kotest:kotest-assertions-core:$koTestCoreVersion")
+    testImplementation("io.kotest:io.kotest:kotest-property:$koTestCoreVersion")
+}
+
+tasks.withType<Test>().configureEach {
+    useJUnitPlatform()
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
