b74554b2a6a55b0e36c57516dce6ca6d9ad8eae2
Boris Prochnau <boris.prochnau@colenio.com>
2024-02-27 12:20:18 +0100

    feature(coroutine): add classical multi-thread example

---
+++ Coroutines/src/main/kotlin/Main.kt
@@ -0,0 +1,11 @@
+import kotlin.concurrent.thread
+
+fun main() {
+    println("I'm on ${Thread.currentThread().name}")
+    thread { //
+        println("And I'm on ${Thread.currentThread().name}")
+    }
+}
+
+// I'm on main
+// And I'm on Thread-0
